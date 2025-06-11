8af89ef703389750262820aa4873d33138c2d108
remy-sigiq <remy.white@sigiq.ai>
2025-06-11 11:38:06 -0700

    test

--- src/nodes/index.ts
+++ src/nodes/index.ts
@@ -5,7 +5,7 @@ import { AppNode } from './types';
 import ToolbarNode from './ToolbarNode';
 
 export const initialNodes: AppNode[] = [
-  { id: 'a', type: 'input', position: { x: 0, y: 0 }, data: { label: 'autodeploy worked' } },
+  { id: 'a', type: 'input', position: { x: 0, y: 0 }, data: { label: 'autodeploy worked 2' } },
   {
     id: 'b',
     type: 'position-logger',
